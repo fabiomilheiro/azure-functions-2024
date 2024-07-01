bb8dc35537e1889ff4b3973cecafcf51c87c73b4
Fabio Milheiro <fabiomilheiro@gmail.com>
2024-07-01 20:54:39 +0100

    Different settings per environment

--- UserService/Startup.cs
+++ UserService/Startup.cs
@@ -1,20 +1,36 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserService.Helpers;
 
 [assembly: FunctionsStartup(typeof(UserService.Startup))]
 namespace UserService
 {
     public class Startup : FunctionsStartup
     {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var context = builder.GetContext();
+
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "settings.json"))
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"settings.{context.EnvironmentName}.json"));
+        }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddSingleton<IExampleService, DefaultExampleService>();
+            builder.Services.AddOptions<Settings>().Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.Bind(settings);
+            });
         }
     }
 }
