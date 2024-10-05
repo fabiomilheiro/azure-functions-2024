3957ccad62c43292759bba320973b46237067b03
Fabio Milheiro <fabiomilheiro@gmail.com>
2024-10-05 18:42:42 +0100

    move

---
+++ azf.UserService/Sql/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Azf.Shared.Sql.Models;
+using System;
+
+namespace Azf.UserService.Sql.Models
+{
+    public class User : ICreatedAt, IUpdatedAt
+    {
+        public required Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+    }
+}
